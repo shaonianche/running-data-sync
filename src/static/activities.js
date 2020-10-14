1feaf7d2f96b2dbc22d7c34d9a1c3e17757a0604
shaonianche <duanfei.job@gmail.com>
2020-10-14 06:24:50 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -672,6 +672,20 @@ const activities = [
     "streak": 2
   },
   {
+    "run_id": 4172140514,
+    "name": "\u665a\u95f4\u8dd1\u6b65",
+    "distance": 1968.9,
+    "moving_time": "0:13:37",
+    "type": "Run",
+    "start_date": "2020-10-09 14:40:02+00:00",
+    "start_date_local": "2020-10-09 22:40:02",
+    "location_country": "\u5e38\u8425\u4e2d\u8def, \u679c\u5bb6\u5e97, \u5e38\u8425\u4e61, \u671d\u9633\u533a, \u5317\u4eac\u5e02, 100024, China \u4e2d\u56fd",
+    "summary_polyline": "wmurF}mtfUMQISQOIUMSIY[k@[gBCs@Eo@?[DWA[ZaB`@iAH[D[AUIUMO{ByAc@_@QG}@}@QIoAmAQG]_@q@c@aAy@Q@MPETAV?jADT?\\Cd@Fp@IT?VDhAHt@C`B@VBVCVBXFVDt@Er@BT?ZDVGVBZIv@CnA@XHZEpA@ZDTGv@Hn@?t@C\\Bv@Av@Bt@TJRB|@Bh@?d@Ez@JfCv@NHPDRLRBPLz@Vd@L|@DTIR@REf@CnAg@NMRKHSA]OiAIU@WMS",
+    "average_heartrate": 136.3,
+    "average_speed": 2.41,
+    "streak": 1
+  },
+  {
     "run_id": 1602254402001,
     "name": "run from gpx",
     "distance": 1936.8249144097717,
