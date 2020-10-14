56f098afccaac01a8ec491afc92fa3d3774cfa5f
duanfei <duanfei.job@gmail.com>
2020-10-14 10:47:44 +0800

    fix($siteUrl):update siteUrl "https://run.duangfei.org/"

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   siteMetadata: {
     title: "Running page",
-    siteUrl: "run.duangfei.org",
+    siteUrl: "https://run.duangfei.org/",
     description: "Personal site and blog",
   },
   plugins: [
