1619d46090724503cde77b3893131584717a4701
duanfei <duanfei.job@gmail.com>
2020-10-15 14:02:05 +0800

    chore:update nike scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   siteMetadata: {
     title: "Running page",
-    siteUrl: "https://run.duangfei.org/",
+    siteUrl: "https://yihong.run",
     description: "Personal site and blog",
   },
   plugins: [
