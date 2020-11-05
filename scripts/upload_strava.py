ca3586e1bfd8a4903c4eb824339cb1dc4d8e8fa6
duanfei <duanfei.job@gmail.com>
2020-11-05 22:29:21 +0800

    fix: delete upload_strava,py make_new_gpxs

--- scripts/upload_strava.py
+++ scripts/upload_strava.py
@@ -1,14 +1,14 @@
-from datetime import datetime, timedelta
-import time
-import logging
+import argparse
 import json
-from json.decoder import JSONDecodeError
+import logging
 import os
 import os.path
+import time
+from datetime import datetime, timedelta
+from json.decoder import JSONDecodeError
 from xml.etree import ElementTree
-import argparse
-import gpxpy.gpx
 
+import gpxpy.gpx
 from stravalib.client import Client
 
 logging.basicConfig(level=logging.INFO)
@@ -168,49 +168,50 @@ def parse_activity_data(activity):
     )
     return gpx_doc
 
-
-def save_gpx(gpx_data, activity_id):
-    """
-    Saves the GPX data to a file on disk
-    Args:
-        gpx_data: the GPX XML doc
-        activity_id: the name of the file
-    """
-
-    file_path = os.path.join(GPX_FOLDER, activity_id + ".gpx")
-    with open(file_path, "w") as f:
-        f.write(gpx_data)
-
-
 def upload_gpx(file_name):
     with open(file_name, "rb") as f:
         r = client.upload_activity(activity_file=f, data_type="gpx")
         print(r)
 
+# def save_gpx(gpx_data, activity_id):
+#     """
+#     Saves the GPX data to a file on disk
+#     Args:
+#         gpx_data: the GPX XML doc
+#         activity_id: the name of the file
+#     """
 
-def make_new_gpxs(files):
-    # TODO refactor maybe we do not need to upload
-    if not files:
-        return
-    if not os.path.exists(GPX_FOLDER):
-        os.mkdir(GPX_FOLDER)
-    for file in files:
-        with open(file, "r") as f:
-            try:
-                json_data = json.loads(f.read())
-            except JSONDecodeError:
-                pass
-        # ALL save name using utc if you want local please offset
-        gpx_name = str(datetime.utcfromtimestamp(int(json_data["start_epoch_ms"]) / 1000).strftime('%Y-%m-%d %H-%M-%S'))
-        parsed_data = parse_activity_data(json_data)
-        if parsed_data:
-            save_gpx(parsed_data, gpx_name)
-    gpx_files = sorted(os.listdir(GPX_FOLDER))
-    # get new, TODO: not mind the delete stai
-    gpx_files = gpx_files[-len(files):]
-    for f in gpx_files:
-        upload_gpx(os.path.join(GPX_FOLDER, f))
-        logger.info(f +" uploaded")
+#     file_path = os.path.join(GPX_FOLDER, activity_id + ".gpx")
+#     with open(file_path, "w") as f:
+#         f.write(gpx_data)
+
+
+
+
+
+# def make_new_gpxs(files):
+#     # TODO refactor maybe we do not need to upload
+#     if not files:
+#         return
+#     if not os.path.exists(GPX_FOLDER):
+#         os.mkdir(GPX_FOLDER)
+#     for file in files:
+#         with open(file, "r") as f:
+#             try:
+#                 json_data = json.loads(f.read())
+#             except JSONDecodeError:
+#                 pass
+#         # ALL save name using utc if you want local please offset
+#         gpx_name = str(datetime.utcfromtimestamp(int(json_data["start_epoch_ms"]) / 1000).strftime('%Y-%m-%d %H-%M-%S'))
+#         parsed_data = parse_activity_data(json_data)
+#         if parsed_data:
+#             save_gpx(parsed_data, gpx_name)
+#     gpx_files = sorted(os.listdir(GPX_FOLDER))
+#     # get new, TODO: not mind the delete stai
+#     gpx_files = gpx_files[-len(files):]
+#     for f in gpx_files:
+#         upload_gpx(os.path.join(GPX_FOLDER, f))
+#         logger.info(f +" uploaded")
 
 
 if __name__ == "__main__":
@@ -234,4 +235,3 @@ if __name__ == "__main__":
     # change here to manual
     time.sleep(5)
     files = get_to_generate_files(last_time)
-    make_new_gpxs(files)
