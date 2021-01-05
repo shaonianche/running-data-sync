1ccd350f39defbef1aa70bc0942b338a8b506dcb
duanfei <duanfei.job@gmail.com>
2021-01-05 16:41:23 +0800

    fix: No module named config.yaml, Commenting out yarn file judgments

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,5 @@
 import os
+
 import yaml
 
 GET_DIR = "activities"
@@ -12,20 +13,20 @@ BASE_URL = "https://api.nike.com/sport/v3/me"
 TOKEN_REFRESH_URL = "https://unite.nike.com/tokenRefresh"
 NIKE_CLIENT_ID = "HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH"
 
-try:
-    with open("config.yaml") as f:
-        _config = yaml.safe_load(f)
-except:
-    _config = {}
+# try:
+#     with open("config.yaml") as f:
+#         _config = yaml.safe_load(f)
+# except:
+#     _config = {}
 
 
-def config(*keys):
-    def safeget(dct, *keys):
-        for key in keys:
-            try:
-                dct = dct[key]
-            except KeyError:
-                return None
-        return dct
+# def config(*keys):
+#     def safeget(dct, *keys):
+#         for key in keys:
+#             try:
+#                 dct = dct[key]
+#             except KeyError:
+#                 return None
+#         return dct
 
-    return safeget(_config, *keys)
+#     return safeget(_config, *keys)
