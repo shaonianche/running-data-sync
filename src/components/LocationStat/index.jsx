fc7cde946103039c2b92931e2f6a6c69c84b3eaa
friendsa <duanfei.job@gmail.com>
2021-10-21 21:09:32 +0800

    fix:some error

--- src/components/LocationStat/index.jsx
+++ src/components/LocationStat/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import YearStat from 'src/components/YearStat';
-import {
-  CHINESE_LOCATION_INFO_MESSAGE_FIRST,
-  CHINESE_LOCATION_INFO_MESSAGE_SECOND
-} from '../../utils/const';
 import CitiesStat from './CitiesStat';
 import LocationSummary from './LocationSummary';
 import PeriodStat from './PeriodStat';
@@ -11,14 +7,7 @@ import PeriodStat from './PeriodStat';
 const LocationStat = ({ changeYear, changeCity, changeTitle }) => (
   <div className="fl w-100 w-30-l pb5 pr5-l">
     <section className="pb4" style={{ paddingBottom: '0rem' }}>
-      <p>
-        {CHINESE_LOCATION_INFO_MESSAGE_FIRST}.
-        <br />
-        {CHINESE_LOCATION_INFO_MESSAGE_SECOND}.
-        <br />
-        <br />
-        Yesterday you said tomorrow.
-      </p>
+      <p>Yesterday you said tomorrow.</p>
     </section>
     <hr color="red" />
     <LocationSummary />
