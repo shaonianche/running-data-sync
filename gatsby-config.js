bdaea491853c5b1973b7354a124da3ca83e0f2f1
duanfei <duanfei.job@gmail.com>
2022-06-01 12:34:45 +0800

    clear code #482

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,47 +18,45 @@ module.exports = {
   },
 
   plugins: [
-    "gatsby-transformer-json",
+    'gatsby-transformer-json',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "data",
-        path: "./src/static",
+        path: './src/static/',
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images",
+        path: './src/images/',
       },
     },
     {
-      resolve: "gatsby-alias-imports",
+      resolve: 'gatsby-alias-imports',
       options: {
-        rootFolder: "./",
+        rootFolder: './',
       },
     },
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          "gatsby-remark-responsive-iframe",
-          "gatsby-remark-smartypants",
-          "gatsby-remark-widows",
-          "gatsby-remark-external-links",
+          'gatsby-remark-responsive-iframe',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-widows',
+          'gatsby-remark-external-links',
           {
-            resolve: "gatsby-remark-autolink-headers",
+            resolve: 'gatsby-remark-autolink-headers',
             options: {
-              className: "header-link",
+              className: 'header-link',
             },
           },
         ],
       },
     },
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: 'gatsby-plugin-sass',
       options: {
         cssLoaderOptions: {
           esModule: false,
@@ -69,55 +67,33 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /assets/,
         },
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-transformer-sharp",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        // The option defaults to true
-        checkSupportedExtensions: true,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#e1e1e1",
-        theme_color: "#e1e1e1",
-        display: "minimal-ui",
-        icon: "src/images/logo-favicon.png", // This path is relative to the root of the site.
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        policy: [{ userAgent: "*", allow: "/" }],
-      },
-    },
-    // {
-    //   resolve: 'gatsby-plugin-firebase',
-    //   options: {
-    //     credentials: {
-    //       apiKey: 'AIzaSyDjHBC567IJXStrdCrXxpmd7hdwHSN-eMY',
-    //       authDomain: 'running-data-sync.firebaseapp.com',
-    //       // databaseURL: 'running-data-sync.firebaseapp.com',
-    //       projectId: 'running-data-sync',
-    //       storageBucket: 'running-data-sync.appspot.com',
-    //       messagingSenderId: '967054291585',
-    //       appId: '1:967054291585:web:ea3c6b820c0ff2373c9380',
-    //       measurementId: 'G-1RMYP62RLZ'
-    //     }
-    //   }
-    // }
   ],
 };
