0ad71493882fbb00e7a04c07ac39825ccae574cd
Friends A <duanfei.job@gmail.com>
2022-06-10 16:19:56 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,8 +63,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#e1e1e1',
-        theme_color: '#e1e1e1',
+        background_color: '#1A1A1A',
+        theme_color: '#1A1A1A',
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
