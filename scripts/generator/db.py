aa33e2fb805b941feaf8bc7e43108b646c309a4d
Friends A <github@duanfei.org>
2022-09-21 15:29:57 +0800

    Update db.py

--- scripts/generator/db.py
+++ scripts/generator/db.py
@@ -1,3 +1,4 @@
+
 import datetime
 import random
 import string
@@ -80,7 +81,8 @@ def update_or_create_activity(session, run_activity):
         if not activity:
             start_point = run_activity.start_latlng
             location_country = getattr(run_activity, "location_country", "")
-            if not location_country and start_point:
+            # or China for #176 to fix
+            if not location_country and start_point or location_country == "China":
                 try:
                     location_country = str(
                         g.reverse(f"{start_point.lat}, {start_point.lon}")
@@ -111,6 +113,7 @@ def update_or_create_activity(session, run_activity):
                 summary_polyline=(
                     run_activity.map and run_activity.map.summary_polyline or ""
                 ),
+            )
             session.add(activity)
             created = True
         else:
@@ -121,9 +124,9 @@ def update_or_create_activity(session, run_activity):
             activity.type = run_activity.type
             activity.average_heartrate = run_activity.average_heartrate
             activity.average_speed = float(run_activity.average_speed)
-            summary_polyline=(
-                 run_activity.map and run_activity.map.summary_polyline or ""
-                ),
+            activity.summary_polyline = (
+                run_activity.map and run_activity.map.summary_polyline or ""
+            )
     except Exception as e:
         print(f"something wrong with {run_activity.id}")
         print(str(e))
