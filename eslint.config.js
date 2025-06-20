345aa2be9575241c9ea0dd4f4b972d0a1bdedb69
shaonianche <github@duanfei.org>
2025-06-21 01:27:52 +0800

    chore: update eslint configuration and package.json for improved file handling

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   react: true,
-  ignorePatterns: ['pyproject.toml', 'scripts/*'],
+  ignores: ['src/static/activities.json'],
 })
