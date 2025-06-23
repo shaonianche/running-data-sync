cb566df31f4d06e4bec0d0d103bea8ae6e57b9a6
shaonianche <github@duanfei.org>
2025-06-23 17:13:26 +0800

    update html meta

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header'
 import getSiteMetadata from '@/hooks/useSiteMetadata'
 
 function Layout({ children }: React.PropsWithChildren) {
-  const { siteTitle, description, siteUrl, logo } = getSiteMetadata()
+  const { siteTitle, description } = getSiteMetadata()
 
   return (
     <>
@@ -17,20 +17,6 @@ function Layout({ children }: React.PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:image" content={logo} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:site_name" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={siteTitle} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={logo} />
-
       </Helmet>
       <Header />
       <div className="max-w-7xl mx-auto mb-16 p-4 lg:flex lg:p-16">{children}</div>
