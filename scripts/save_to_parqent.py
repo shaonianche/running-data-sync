96d713c587bafdbbdfe2edee5d91ae1f88c77cb6
shaonianche <github@duanfei.org>
2025-06-29 20:23:02 +0800

    fix: ensure directory exists before saving activities to parquet

--- scripts/save_to_parqent.py
+++ scripts/save_to_parqent.py
@@ -1,9 +1,19 @@
+import os
+
 import duckdb
 from config import DB_FOLDER, SQL_FILE
 
+if not os.path.exists(DB_FOLDER):
+    os.makedirs(DB_FOLDER)
+
 # The SQL_FILE is now a duckdb file
 with duckdb.connect(SQL_FILE) as conn:
-    conn.sql(
-        f"COPY (SELECT * FROM activities) TO '{DB_FOLDER}/activities.parquet'"
-        f" (FORMAT PARQUET);"
-    )
+    try:
+        conn.sql(
+            f"COPY (SELECT * FROM activities) TO '{DB_FOLDER}/activities.parquet'"
+            f" (FORMAT PARQUET);"
+        )
+    except Exception as e:
+        print(f"Error saving to parquet: {e}")
+        # if there is no activities table, just ignore
+        pass
